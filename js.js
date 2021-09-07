33e1c9c9201524c846e1fa12cc65a3ff586df666
Mobinul Islam Mahi <71390423+Mobinulislammahi@users.noreply.github.com>
2021-09-07 18:19:49 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+var textWrapper = document.querySelector('.some');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.some .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.some',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+	
